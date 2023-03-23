2f36ce0ccc9fbbd00359293c982554a217aa0784
Alperen ELMAS <bilgeelmas10076@gmail.com>
2023-03-23 16:32:34 +0300

    hepsi

--- EnemyController.cs
+++ EnemyController.cs
@@ -6,22 +6,22 @@ using System;
 public class EnemyController : MonoBehaviour
 {
     public GameObject disk;
-    public float timer;
+    public float timer_two;
     public int control;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        timer += Time.deltaTime;
+        timer_two += Time.deltaTime;
 
-        if (Convert.ToInt32(timer) % 2 == 0)
+        if (Convert.ToInt32(timer_two) % 2 == 0)
         {
-            if (control < Convert.ToInt32(timer))
+            if (control < Convert.ToInt32(timer_two))
             {
                 Instantiate(disk, gameObject.transform.position, Quaternion.identity);
 
             }
-            control = Convert.ToInt32(timer) + 1;
+            control = Convert.ToInt32(timer_two) + 1;
         }
 
 
